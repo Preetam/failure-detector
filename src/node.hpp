fbd885f230fe2d2b3eee1adf4df120a3246ed297
Preetam Jinka <pj@preet.am>
2015-09-24 12:26:18 -0400

    clean up

--- src/node.hpp
+++ src/node.hpp
@@ -52,13 +52,11 @@ private:
 	{
 		auto peer = std::make_shared<Peer>(id_counter, std::move(conn_ptr),
 			std::packaged_task<void(int)>([this](int id) {
-			std::cerr << "connection " << id << " closed." << std::endl;
 			peers->erase(id);
 		}));
 		threads.push_back(std::make_unique<std::thread>([peer]() {
 			peer->run();
 		}));
-		std::cerr << "asdf" << std::endl;
 		peers->insert({id_counter, peer});
 	}
 
--- src/peer.hpp
+++ src/peer.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <memory>
-#include <thread>
 #include <iostream>
 #include <future>
 
